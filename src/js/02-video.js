8cb684270569e3cb6d990086986946b938e3d053
Denys Volodko <41117606+dvolodko@users.noreply.github.com>
2022-12-03 15:20:23 +0200

    video task in proccess

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import { save, load, remove } from "./storage";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let currentTime;
+
+const SavingCurrentTimeToLocalStorage = function (data) {
+    currentTime = localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+}
+
+player.setCurrentTime(currentTime).then
+
+player.on('timeupdate', SavingCurrentTimeToLocalStorage);
