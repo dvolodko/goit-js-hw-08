656e4bf676e74e5f14f0155eb5c5a1be6f37db7f
Denys Volodko <41117606+dvolodko@users.noreply.github.com>
2022-12-03 15:42:48 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import Player from '@vimeo/player';
-import { save, load, remove } from "./storage";
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -8,9 +8,11 @@ const savingCurrentTimeToLocalStorage = function (data) {
     localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
 }
 
+const throttledSavingCurrentTime = throttle(savingCurrentTimeToLocalStorage, 1000);
+
 const currentTimeFromLocalStorage = localStorage.getItem("videoplayer-current-time");
 const parsedCurrentTimeFromLocalStorage = JSON.parse(currentTimeFromLocalStorage);
 
 player.setCurrentTime(parsedCurrentTimeFromLocalStorage);
 
-player.on('timeupdate', savingCurrentTimeToLocalStorage);
+player.on('timeupdate', throttledSavingCurrentTime);
