622982229c6465bb37a1025daf756a0c1a15c7c1
Denys Volodko <babyleejane@gmail.com>
2022-12-03 22:57:57 +0200

    task 3 in proccess

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+import { save, load, remove } from './storage';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const refs = {
+  form: document.querySelector('form'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', onFormInput);
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+  console.log(formData);
+  save(STORAGE_KEY, formData);
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  remove(STORAGE_KEY);
+}
